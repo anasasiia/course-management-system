06c8b817369a42db8567e6b00cc57fe3f13dbcb6
anasasiia <sheldoncooperbuga@gmail.com>
2023-08-06 16:10:25 +0400

    add authentication

--- app/src/main/java/com/example/app/config/security/SecurityConfig.java
+++ app/src/main/java/com/example/app/config/security/SecurityConfig.java
@@ -1,22 +1,88 @@
 package com.example.app.config.security;
 
-import org.springframework.context.annotation.Bean;
+import com.example.app.component.JWTHelper;
+import com.example.app.filter.JWTAuthenticationFilter;
+import com.example.app.filter.JWTAuthorizationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String LOGIN = "/login";
+
+    public static final List<GrantedAuthority> DEFAULT_AUTHORITIES = List.of(new SimpleGrantedAuthority("USER"));
+
+    private final RequestMatcher publicUrls;
+    private final RequestMatcher loginRequest;
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTHelper jwtHelper;
+
+    public SecurityConfig(@Value("${base-url}") final String baseUrl,
+                          final UserDetailsService userDetailsService,
+                          final PasswordEncoder passwordEncoder,
+                          final JWTHelper jwtHelper) {
+        this.loginRequest = new AntPathRequestMatcher(baseUrl + LOGIN, POST.toString());
+        this.publicUrls = new OrRequestMatcher(
+                loginRequest,
+                new NegatedRequestMatcher(new AntPathRequestMatcher(baseUrl + "/**"))
+        );
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtHelper = jwtHelper;
+    }
+
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+
+        final var authenticationFilter = new JWTAuthenticationFilter(
+                authenticationManagerBean(),
+                loginRequest,
+                jwtHelper
+        );
+
+        final var authorizationFilter = new JWTAuthorizationFilter(
+                publicUrls,
+                jwtHelper
+        );
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/webjars/**").permitAll()
+                .requestMatchers(publicUrls).permitAll()
+                .anyRequest().authenticated()
                 .and()
-                .csrf()
-                .disable();
-        return http.build();
+                .addFilter(authenticationFilter)
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .logout().disable();
     }
 }
